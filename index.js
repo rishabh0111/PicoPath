372c483f2e8a55448747103f4749faa4352eeb00
rishabh0111 <rishabhsharma8912@gmail.com>
2024-01-24 15:28:37 +0530

    Implemented Statefull authentication

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const { connectToMongoDB } = require('./connect');
+const { restrictToLoggedInUserOnly, checkAuth } = require('./middlewares/auth')
+
+const URL = require('./models/url');
 
 const urlRoute = require('./routes/url');
 const staticRoute = require('./routes/staticRouter')
-const URL = require('./models/url');
+const userRoute = require('./routes/user')
+
 const app = express();
 const PORT = 8001;
 
@@ -16,10 +21,11 @@ app.set('views', path.resolve('./views'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
-app.use("/url", urlRoute);
-
-app.use('/', staticRoute);
+app.use("/url", restrictToLoggedInUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use('/', checkAuth, staticRoute);
 
 app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
