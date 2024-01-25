e069f89d2fcd90d725788fe5d2b60970de9a2966
rishabh0111 <rishabhsharma8912@gmail.com>
2024-01-25 22:27:45 +0530

    implemented stateless authentication using jwt

--- controllers/user.js
+++ controllers/user.js
@@ -20,9 +20,8 @@ async function handleUserLogin(req, res) {
             error: 'Invalid Username or Password',
         });
 
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie('uid', sessionId);
+    const token = setUser(user);
+    res.cookie('uid', token);
     return res.redirect("/");
 }
 
--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,20 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken');
+const secret = "Rish$123@$";
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+function setUser(user) {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret);
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+    if(!token) return null;
+    try {
+        return jwt.verify(token, secret);
+    } catch(error){
+        return null;
+    }
 }
 
 module.exports = {
